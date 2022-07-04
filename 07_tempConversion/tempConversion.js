ed26340e60280c59a6ca3bd2e468b3804e9e143e
melumilic <melumilic@gmail.com>
2022-07-03 23:04:42 -0700

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(number) {
+  celsius = (number-32)*5.0/9;
+  return Math.round(celsius*10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(number) {
+  fahrenheit=(number*9.0/5)+32;
+  return Math.round(fahrenheit*10)/10;
 };
 
 // Do not edit below this line
