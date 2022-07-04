5e4ea7bd39806287c1968a4d92557b940023a34b
melumilic <melumilic@gmail.com>
2022-07-03 23:36:18 -0700

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	arr = numbers;
+  var sum = 0;
+  // for (let index = 0; index < nums.length; index++) {
+  //   sum = sum +nums[index];
+  // }
+  // return sum;
+  return arr.reduce((a, b) => a+b,0)
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factorial = 1;
+	for(let i = 1; i<=a;i++){
+    factorial*=i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
