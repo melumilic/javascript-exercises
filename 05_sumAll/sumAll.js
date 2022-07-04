73efbce5ab9d2f9a6eee899dbec26b5a4553fc4b
melumilic <melumilic@gmail.com>
2022-07-03 22:50:42 -0700

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function (numStart, numEnd) {
+    if(typeof(numStart)!=="number"||typeof(numEnd)!=="number"||numStart<0||numEnd<0){
+        return "ERROR";
+    }
+  return Math.abs(
+    (numEnd * (numEnd + 1)) / 2 - (numStart * (numStart + 1)) / 2 
+  )+1;
 };
 
 // Do not edit below this line
