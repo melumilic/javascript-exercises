c0ba45c2686ce9607f433a5a1cfb2443b3574111
melumilic <melumilic@gmail.com>
2022-07-03 22:39:01 -0700

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...args) {
+  let temp = [...arr];
+  let things = [...args];
+  for (let i = 0; i < args.length; i++) {
+    let testVar = temp.indexOf(things[i]);
+    if(testVar===-1){
+        continue;
+    }
+    temp.splice(testVar, 1);
+  }
+  return temp;
 };
 
 // Do not edit below this line
