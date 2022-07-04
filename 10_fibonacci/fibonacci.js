51e58d5405932fce33d42e51ac744604eadae7cc
melumilic <melumilic@gmail.com>
2022-07-04 00:20:15 -0700

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num<0){
+        return "OOPS";
+    }
+    if(typeof(num)==="string"){
+        num = parseInt(num);
+    }
+    let a = 0;
+    let b = 1;
+    let fib = 1;
+    for(let i = 1;i<num;i++){
+        fib=(a+b);
+        a=b;
+        b=fib;
+    }
+    return fib;
 };
 
 // Do not edit below this line
