aa1ed4fd16a14837ddadc03178c19068f7e68b02
melumilic <melumilic@gmail.com>
2022-07-04 00:01:24 -0700

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let regex = /\p{P}/gu;
+    let cleanString = string.replace(/[^\p{L}\p{N}\s]/gu, '');
+    cleanString = cleanString.toLowerCase().replace(/ /g,"");
+    let center = Math.floor(cleanString.length/2);
+    if(cleanString.length%2===0){
+        return cleanString.substr(0,center)===cleanString.substr(center).split("").reverse().join("");
+    }else{
+        return cleanString.substr(0,center)===cleanString.substr(center+1).split("").reverse().join("");
+    }
+    
 };
 
 // Do not edit below this line
