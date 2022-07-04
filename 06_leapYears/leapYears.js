6548c4f11915a10ef91e785dc2f33c242a6c0262
melumilic <melumilic@gmail.com>
2022-07-03 22:54:17 -0700

    Complete exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function(number) {
+    leapTest = number%4===0&&(number%100!==0||number%400===0);
+    return leapTest;
 };
 
 // Do not edit below this line
